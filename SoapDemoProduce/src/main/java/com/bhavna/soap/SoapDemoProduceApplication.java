511d71d02f6892536ba08d7859e7799d61a8108e
SrikanthNanaboina <srikanth.nanaboina@bhavnacorp.com>
2022-02-24 23:53:47 +0530

    Soap Task

---
+++ SoapDemoProduce/src/main/java/com/bhavna/soap/SoapDemoProduceApplication.java
@@ -0,0 +1,13 @@
+package com.bhavna.soap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapDemoProduceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapDemoProduceApplication.class, args);
+	}
+
+}
