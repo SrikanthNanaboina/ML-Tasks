2eda5347bd6140c982ccc19b2ebb52db276248be
SrikanthNanaboina <srikanth.nanaboina@bhavnacorp.com>
2022-03-02 10:30:29 +0530

    Custom Exception

--- OrderManagement/src/main/java/com/bhavna/exceptionhandler/ControllerExceptionHandler.java
+++ OrderManagement/src/main/java/com/bhavna/exceptionhandler/ControllerExceptionHandler.java
@@ -1,20 +1,25 @@
 package com.bhavna.exceptionhandler;
 
+import java.util.Date;
+import java.util.NoSuchElementException;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
-//	@ExceptionHandler(ResourceNotFoundException.class)
-//	  public ResponseEntity<ErrorMessage> resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
-//	    ErrorMessage message = new ErrorMessage(
-//	        HttpStatus.NOT_FOUND.value(),
-//	        new Date(),
-//	        ex.getMessage(),
-//	        request.getDescription(false));
-//	    
-//	    return new ResponseEntity<ErrorMessage>(message, HttpStatus.NOT_FOUND);
-//	  }
+	@ExceptionHandler(NoSuchElementException.class)
+	  public ResponseEntity<ErrorMessage> resourceNotFoundException(NoSuchElementException ex, WebRequest request) {
+	    ErrorMessage message = new ErrorMessage(
+	        HttpStatus.NOT_FOUND.value(),
+	        new Date(),
+	        ex.getMessage(),
+	        request.getDescription(false));
+	    
+	    return new ResponseEntity<ErrorMessage>(message, HttpStatus.NOT_FOUND);
+	  }
 
 }
